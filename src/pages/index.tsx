2917d0558e6636fb8f04da8f3dcc10bbe17cb8c1
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-06-06 09:58:58 -0300

    Adiciona hr e mensagem principal na tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Center, Container, Divider, Heading } from "@chakra-ui/react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import TravelTypes from "../components/TravelTypes";
@@ -8,6 +9,25 @@ export default function Home() {
       <Header />
       <Banner />
       <TravelTypes />
+
+      <Center width={90} margin="auto" marginTop="80px">
+        <Divider
+          orientation="horizontal"
+          borderColor="dark.headingsAndTexts"
+          borderWidth={2}
+        />
+      </Center>
+      <Center marginTop="52px">
+        <Heading
+          textAlign="center"
+          fontWeight="500"
+          color="dark.headingsAndTexts"
+          wordBreak="break-word"
+        >
+          Vamos nessa? <br />
+          Então escolha seu continente
+        </Heading>
+      </Center>
     </>
   );
 }
